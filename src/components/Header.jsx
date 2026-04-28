44a94c4786f76c65acde5866d07b022b3be92b7a
Jayanti Thakor <techeducator8@gmail.com>
2026-04-28 15:38:16 +0530

    product page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { motion } from "framer-motion";
 import "./style.css";
 import { useNavigate } from "react-router-dom";
+import { signOut, onAuthStateChanged } from "firebase/auth";
+import { auth } from "../firebase";
 
 const Header = () => {
+  const [user, setUser] = useState(null);
+  const [open, setOpen] = useState(false);
 
   const navigate = useNavigate();
+
+  const logout = async () => {
+    await signOut(auth);
+    localStorage.removeItem("user-auth-data");
+    alert("Logout successfully!");
+  };
+
+  useEffect(() => {
+    const stop = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
+
+    return () => stop();
+  }, []);
 
   return (
     <header className="header">
@@ -21,7 +39,7 @@ const Header = () => {
       <nav className="navbar">
         <ul>
           <li><a href="/">Home</a></li>
-          <li><a href="/">Shop</a></li>
+          <li><a href="/products">Shop</a></li>
           <li><a href="/">Collection</a></li>
           <li><a href="/">About</a></li>
           <li><a href="/">Contact</a></li>
@@ -34,8 +52,26 @@ const Header = () => {
         animate={{ opacity: 1, x: 0 }}
         transition={{ duration: 0.8 }}
       >
-        <button className="login-btn" onClick={()=>navigate('/login')}>Login</button>
-        <button className="shop-btn">Shop Now</button>
+        {user ? (
+          <div className="profile-container">
+            <img
+              src={user.photoURL}
+              alt="profile"
+              className="profile-img"
+              onClick={() => setOpen(!open)}
+            />
+
+            {open && (
+              <div className="dropdown">
+                <p>{user.displayName}</p>
+                <button onClick={() => navigate("/profile")}>Profile</button>
+                <button onClick={logout}>Logout</button>
+              </div>
+            )}
+          </div>
+        ) : (
+          <button onClick={() => navigate("/login")}>Login</button>
+        )}
       </motion.div>
     </header>
   );
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,8 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import api from '../services/api';
 
 const Products = () => {
+
+  const [products,setProducts] = useState([]);
+  const [loading,setLoading] = useState(true);
+  const [error,setError] = useState("");
+
+  const fetchProducts = async()=>{
+    try{
+      const res = await api.get('/products');
+      setProducts(res.data);
+      setLoading(false);
+      setError("");
+    }
+    catch(err){
+      setError("Failed to fetch products.");
+    }
+  }
+
+  useEffect(()=>{
+    fetchProducts();
+  },[])
+
+  if(error) return <h2>{error}</h2>;
+  if(loading) return <h2>Loading...</h2>
   return (
-    <div>Products</div>
+    <div>
+      {
+        products.length === 0 ? <h2>Product Not Found.!</h2>:
+        products.map((p)=>(
+          <div className="product-card" key={p.id}>
+            <h3>{p.name}</h3>
+            <img src={p.image} alt="" />
+            <button>View Details</button>
+          </div>
+        ))
+      }
+    </div>
   )
 }
 
