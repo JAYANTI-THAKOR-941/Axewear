8486b790c8de03c1ff73f7c0c49c024d6d082422
JAYANTI-THAKOR-941 <jyantithakor941@gmail.com>
2026-06-03 15:33:47 +0530

    auth Login

--- src/App.jsx
+++ src/App.jsx
@@ -1,58 +1,11 @@
 import React from 'react'
-import { Routes,Route} from 'react-router-dom'
+import LoginForm from './pages/LoginForm'
 
-import Home from './pages/Home'
-import Products from './pages/Products'
-import ProductDescription from './pages/ProductDescription'
-import Login from './pages/Login'
-import Register from './pages/Register'
-import Cart from './pages/Cart'
-import Contact from './pages/Contact'
-import About from './pages/About'
-import Checkout from './pages/Checkout'
-
-import ProductManagement from './admin/ProductManagement'
-import UserManagement from './admin/UserManagement'
-import Dashboard from './admin/Dashboard'
-import Profile from './pages/Profile'
-import PageNotFound from './pages/PageNotFound'
-import Header from './components/Header'
-import Footer from './components/Footer'
-import ProtectedRoute from './components/ProtectedRoute'
 const App = () => {
   return (
-    <>
-    <Header/>
-      <Routes>
-
-        {/* auth routes */}
-        <Route path="/login" element={<Login/>} />
-        <Route path="/register" element={<Register/>} />
-        <Route path="/profile" element={<Profile/>} />
-
-        {/* pubic routes */}
-        <Route path="/" element={<Home/>} />
-        <Route path="/products" element={<Products/>} />
-        <Route path="/product/:id" element={<ProductDescription/>} />
-        <Route path="/cart" element={<Cart/>} />
-        <Route path="/about" element={<About/>} />
-        <Route path="/contact" element={<Contact/>} />
-        <Route path="/checkout" element={<Checkout/>} />
-
-        {/* admin routes */}
-       <Route element={<ProtectedRoute/>}>
-          <Route path="/product-management" element={<ProductManagement/>} />
-          <Route path="/user-management" element={<UserManagement/>} />
-          <Route path="/dashboard" element={<Dashboard/>} />
-       </Route>
-      
-
-
-
-        <Route path="*" element={<PageNotFound/>} />
-
-      </Routes>
-    </>
+    <div>
+      <LoginForm/>
+    </div>
   )
 }
 
